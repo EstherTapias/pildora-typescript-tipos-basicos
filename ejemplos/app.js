19fe472742ca1919d0aa4807cfbd91b9098acf37
EstherTapias <esthertpc93@gmail.com>
2025-07-29 00:38:37 +0200

    change name of file

--- ejemplos/app.js
+++
@@ -1,5 +0,0 @@
-function saludar(nombre) {
-    return "Hola, ".concat(nombre, "!");
-}
-var usuario = "Mundo";
-console.log(saludar(usuario));
--- ejemplos/perfil.js
+++
@@ -1,6 +0,0 @@
-var nombre = "Esther";
-var edad = 27;
-var study = true;
-var hobbies = ["leer", "código", "música"];
-var address = ["Calle Luna", 123];
-console.log("Nombre: ".concat(nombre, ", edad: ").concat(edad, ", Estudiante: ").concat(study, ", Hobbies: ").concat(hobbies.join(", "), ", Direcci\u00F3n: ").concat(address[0], " ").concat(address[1]));
