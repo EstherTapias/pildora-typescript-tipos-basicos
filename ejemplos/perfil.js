b550ef8573adaa4a841a9b16553296797e122485
EstherTapias <esthertpc93@gmail.com>
2025-07-29 00:14:39 +0200

    Add more examples

---
+++ ejemplos/perfil.js
@@ -0,0 +1,6 @@
+var nombre = "Esther";
+var edad = 27;
+var study = true;
+var hobbies = ["leer", "código", "música"];
+var address = ["Calle Luna", 123];
+console.log("Nombre: ".concat(nombre, ", edad: ").concat(edad, ", Estudiante: ").concat(study, ", Hobbies: ").concat(hobbies.join(", "), ", Direcci\u00F3n: ").concat(address[0], " ").concat(address[1]));
---
+++ ejemplos/perfil.ts
@@ -0,0 +1,6 @@
+let nombre: string = "Esther";
+let edad: number = 27;
+let study: boolean = true;
+let hobbies: string[] = ["leer", "código", "música"];
+let address: [string, number] = ["Calle Luna", 123];
+console.log(`Nombre: ${nombre}, edad: ${edad}, Estudiante: ${study}, Hobbies: ${hobbies.join(", ")}, Dirección: ${address[0]} ${address[1]}`);
